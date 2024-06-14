b792409d1654a8a3d19bdd8d2014d6a1575f725d
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-06-14 19:39:02 +0100

    feat: Refactor driver queries for improved readability and maintainability

--- lib/api/drivers/queries.ts
+++ lib/api/drivers/queries.ts
@@ -2,17 +2,15 @@ import { db } from "@/lib/db/index";
 import { type DriverId, driverIdSchema } from "@/lib/db/schema/drivers";
 
 export const getDrivers = async () => {
-  const d = await db.driver.findMany({include: { vehicle: true}});
+  const d = await db.driver.findMany({ include: { vehicle: true } });
   return { drivers: d };
 };
 
 export const getDriverById = async (id: DriverId) => {
   const { id: driverId } = driverIdSchema.parse({ id });
   const d = await db.driver.findFirst({
-    where: { id: driverId},
-    include: { vehicle: true }
+    where: { id: driverId },
+    // include: { vehicle: true }
   });
   return { driver: d };
 };
-
-
